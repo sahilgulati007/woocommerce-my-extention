ad0d698ccf8a61629e19234af43b01766bec7694
sahil gulati <sahilgulati49@gmail.com>
2019-02-05 14:18:15 +0530

    custom meta box and display value in fornt end

--- woocommerce-my-extension.php
+++ woocommerce-my-extension.php
@@ -48,6 +48,13 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 
         echo '<h2>New Product Tab</h2>';
         echo '<p>Here\'s your new product tab.</p>';
+        global $post;
+
+        // retrieve the global notice for the current post
+        $global_notice = esc_attr( get_post_meta( $post->ID, '_global_notice', true ) );
+
+        $notice = "<div class='sp_global_notice'>$global_notice</div>";
+        echo '<p>'.$notice.'</p>';
 
     }
 
@@ -279,4 +286,101 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
     add_action( 'woocommerce_process_product_meta_simple', 'woocom_save_proddata_custom_fields'  );
 // You can uncomment the following line if you wish to use those fields for "Variable Product Type"
 //add_action( 'woocommerce_process_product_meta_variable', 'woocom_save_proddata_custom_fields'  );
+
+    function global_notice_meta_box() {
+
+        $screens = array( 'product' );
+
+        //fetching post type
+        //$screens = get_post_types();
+
+        foreach ( $screens as $screen ) {
+            add_meta_box(
+                'global-notice',
+                __( 'Global Notice', 'sitepoint' ),
+                'global_notice_meta_box_callback',
+                $screen
+            );
+        }
+    }
+
+    add_action( 'add_meta_boxes', 'global_notice_meta_box' );
+
+    function global_notice_meta_box_callback( $post ) {
+
+        // Add a nonce field so we can check for it later.
+        wp_nonce_field( 'global_notice_nonce', 'global_notice_nonce' );
+
+        $value = get_post_meta( $post->ID, '_global_notice', true );
+
+        echo '<textarea style="width:100%" id="global_notice" name="global_notice">' . esc_attr( $value ) . '</textarea>';
+    }
+
+    /**
+     * When the post is saved, saves our custom data.
+     *
+     * @param int $post_id
+     */
+    function save_global_notice_meta_box_data( $post_id ) {
+
+        // Check if our nonce is set.
+        if ( ! isset( $_POST['global_notice_nonce'] ) ) {
+            return;
+        }
+
+        // Verify that the nonce is valid.
+        if ( ! wp_verify_nonce( $_POST['global_notice_nonce'], 'global_notice_nonce' ) ) {
+            return;
+        }
+
+        // If this is an autosave, our form has not been submitted, so we don't want to do anything.
+        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+            return;
+        }
+
+        // Check the user's permissions.
+        if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+            if ( ! current_user_can( 'edit_page', $post_id ) ) {
+                return;
+            }
+
+        }
+        else {
+
+            if ( ! current_user_can( 'edit_post', $post_id ) ) {
+                return;
+            }
+        }
+
+        /* OK, it's safe for us to save the data now. */
+
+        // Make sure that it is set.
+        if ( ! isset( $_POST['global_notice'] ) ) {
+            return;
+        }
+
+        // Sanitize user input.
+        $my_data = sanitize_text_field( $_POST['global_notice'] );
+
+        // Update the meta field in the database.
+        update_post_meta( $post_id, '_global_notice', $my_data );
+    }
+
+    add_action( 'save_post', 'save_global_notice_meta_box_data' );
+
+//    function global_notice_before_post( $content ) {
+//
+//        global $post;
+//
+//        // retrieve the global notice for the current post
+//        $global_notice = esc_attr( get_post_meta( $post->ID, '_global_notice', true ) );
+//
+//        $notice = "<div class='sp_global_notice'>$global_notice</div>";
+//
+//        return $notice . $content;
+//
+//    }
+//
+//    add_filter( 'the_content', 'global_notice_before_post' );
 }
